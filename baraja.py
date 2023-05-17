0727df0520c25c2cd402c68139648b0aead022f1
Riload <ricardovlmmarcos@gmail.com>
2023-05-17 21:29:05 +0200

    Funciones de clase

---
+++ baraja.py
@@ -0,0 +1,20 @@
+import random
+
+def crear_baraja(n, p):
+    numeros = n
+    palos = p
+
+    baraja = []
+    for palo in palos:
+        for numero in numeros:
+            naipe = numero + palo
+            baraja.append(naipe)
+
+    return baraja
+
+def barajar_for(baraja) : 
+    for i in range(len(baraja)):
+        j= random.randrange(len(baraja))
+        baraja[i], baraja[j] = baraja[j], baraja[i]
+        
+    return baraja
---
+++ main.py
@@ -0,0 +1,7 @@
+import baraja
+
+print(baraja.crear_baraja(["A", "2", "3"], ("rojos", "verdes")))
+
+b = baraja.crear_baraja("A234567SCR", "oceb")
+print(b)
+baraja.barajar_for(b)
